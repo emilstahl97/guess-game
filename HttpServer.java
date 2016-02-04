99f65a9c77ce58fc395def5a69e6c86d886bacd1
Nina <ninaolo@kth.se>
2016-02-04 10:23:27 +0100

    Add parsing of html delimiter

--- HttpServer.java
+++ HttpServer.java
@@ -1,6 +1,6 @@
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
@@ -11,7 +11,9 @@ import java.util.StringTokenizer;
 public class HttpServer {
 
 	public static void main(String[] args) throws IOException {
+
 		ServerSocket ss = new ServerSocket(4711);
+
 		while (true) {
 			Socket s = ss.accept();
 			BufferedReader request = new BufferedReader(new InputStreamReader(s.getInputStream()));
@@ -34,14 +36,33 @@ public class HttpServer {
 				response.println("Content-Type: image/gif");
 
 			response.println("Set-Cookie: clientId=1; expires=Wednesday,31-Dec-2017 21:00:00 GMT");
-
 			response.println();
+
 			File f = new File("." + requestedDocument);
-			FileInputStream infil = new FileInputStream(f);
-			byte[] b = new byte[1024];
-			while (infil.available() > 0) {
-				response.write(b, 0, infil.read(b));
+			BufferedReader buffReader = new BufferedReader(new FileReader(f.getAbsolutePath()));
+			String firstHalfHtml = "";
+			String content = "";
+			String secondHalfHtml = "";
+			String line = "";
+
+			while (!((line = buffReader.readLine()).contains("/**"))) {
+				firstHalfHtml += line + "\n";
 			}
+
+			while (!((line = buffReader.readLine()).contains("**/"))) {
+				content += line + "\n";
+			}
+
+			while (((line = buffReader.readLine()) != null)) {
+				secondHalfHtml += line + "\n";
+			}
+
+			response.println(firstHalfHtml + secondHalfHtml);
+			System.out.println("FIRST:" + firstHalfHtml);
+			System.out.println("CONTENT:" + content);
+			System.out.println("SECOND:" + secondHalfHtml);
+
+			buffReader.close();
 			s.shutdownOutput();
 			s.close();
 		}
