1d104c75a2ea0eff093e66524c7fbbe0a5969ce8
Nina <ninaolo@kth.se>
2016-02-04 23:58:17 +0100

    Add cookie check

--- HttpServer.java
+++ HttpServer.java
@@ -13,61 +13,80 @@ public class HttpServer {
 	public static void main(String[] args) throws IOException {
 
 		ServerSocket ss = new ServerSocket(4711);
+		int clientCount = 0;
 
 		while (true) {
 			System.out.println("Waiting for requests...");
 			Socket s = ss.accept();
 			BufferedReader request = new BufferedReader(new InputStreamReader(s.getInputStream()));
-			String str = request.readLine();
+			String str = request.readLine(); // GET, POST etc.
 			System.out.println(str);
 			StringTokenizer tokens = new StringTokenizer(str, " ?");
 			tokens.nextToken();
 			String requestedDocument = tokens.nextToken();
 			String token = tokens.nextToken();
+
+			// A user has pressed the submit button
 			if (token.contains("guess=")) {
 				Integer guess = Integer.parseInt(token.split("guess=")[1]);
 				System.out.println("GUESS: " + guess);
 			}
+
+			boolean existingUser = false;
+
 			while ((str = request.readLine()) != null && str.length() > 0) {
+				if (str.contains("Cookie:")) {
+					Integer sessionId = Integer.parseInt(str.split("Cookie: clientId=")[1]);
+					System.out.println("SESSION ID: " + sessionId);
+					existingUser = true;
+				}
 				System.out.println(str);
 			}
 			s.shutdownInput();
 
-			PrintStream response = new PrintStream(s.getOutputStream());
-			response.println("HTTP/1.0 200 OK");
-			response.println("Server : Slask 0.1 Beta");
-			if (requestedDocument.indexOf(".html") != -1)
-				response.println("Content-Type: text/html");
-			if (requestedDocument.indexOf(".gif") != -1)
-				response.println("Content-Type: image/gif");
+			if (existingUser) {
+				// Handle guess
+			} else {
+				// Handle new client
+				PrintStream response = new PrintStream(s.getOutputStream());
+				response.println("HTTP/1.0 200 OK");
+				response.println("Server : Slask 0.1 Beta");
+				if (requestedDocument.indexOf(".html") != -1)
+					response.println("Content-Type: text/html");
+				if (requestedDocument.indexOf(".gif") != -1)
+					response.println("Content-Type: image/gif");
 
-			response.println("Set-Cookie: clientId=1; expires=Wednesday,31-Dec-2017 21:00:00 GMT");
-			response.println();
+				clientCount++;
+				response.println("Set-Cookie: clientId=" + clientCount + ";expires=Wednesday,31-Dec-2017 21:00:00 GMT");
+				response.println();
 
-			File f = new File("." + requestedDocument);
-			BufferedReader buffReader = new BufferedReader(new FileReader(f.getAbsolutePath()));
-			String firstHalfHtml = "";
-			String content = "";
-			String secondHalfHtml = "";
-			String line = "";
+				File f = new File("." + requestedDocument);
+				// Kanske ha en splitHtml()-metod eller nåt...
+				BufferedReader buffReader = new BufferedReader(new FileReader(f.getAbsolutePath()));
+				String firstHalfHtml = "";
+				String content = "";
+				String secondHalfHtml = "";
+				String line = "";
 
-			while (!((line = buffReader.readLine()).contains("/**"))) {
-				firstHalfHtml += line + "\n";
+				while (!((line = buffReader.readLine()).contains("/**"))) {
+					firstHalfHtml += line + "\n";
+				}
+
+				while (!((line = buffReader.readLine()).contains("**/"))) {
+					content += line + "\n";
+				}
+
+				while (((line = buffReader.readLine()) != null)) {
+					secondHalfHtml += line + "\n";
+				}
+
+				response.println(firstHalfHtml + content + secondHalfHtml);
+
+				buffReader.close();
+				s.shutdownOutput();
+				s.close();
 			}
 
-			while (!((line = buffReader.readLine()).contains("**/"))) {
-				content += line + "\n";
-			}
-
-			while (((line = buffReader.readLine()) != null)) {
-				secondHalfHtml += line + "\n";
-			}
-
-			response.println(firstHalfHtml + content + secondHalfHtml);
-
-			buffReader.close();
-			s.shutdownOutput();
-			s.close();
 		}
 	}
 }
